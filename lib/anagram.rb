5d4e75b2c3c9df9b7d9a72d56dd3ea561cac8b89
Jennifer <jenndestes@gmail.com>
2021-02-09 19:24:17 -0600

    commit tree 5a41f07c5b1179255bc1e67d2a59b064169d3cbd

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def original_word
+        @word.split("").sort
+    end
+
+    def word_to_array(word = "")
+        word.split("").sort
+    end
+
+    def match(array)
+        array.select do |word|
+            word_to_array(word) == original_word
+        end
+    end
+end
